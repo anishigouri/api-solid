2a52c5e70e64a78bdf81a7bb51598e6068dbf7b6
Allan Nishigouri <allan.nishigouri@codex.com.br>
2023-11-07 14:05:19 -0300

    Tratamento de erros globais

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -28,7 +28,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       })
     }
 
-    return reply.status(500).send() // TODO: fix me
+    throw err
   }
 
   return reply.status(201).send()
