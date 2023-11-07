42c2e8893f89da5d086bac7f6b3773ea2855b6e5
Allan Nishigouri <allan.nishigouri@codex.com.br>
2023-11-07 16:11:32 -0300

    Utilização do pattern inmemorytestdatabase

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,12 +1,27 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 import { RegisterService } from './register'
-import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Service', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+
+    const registerService = new RegisterService(usersRepository)
+
+    const { user } = await registerService.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
   it('should dash use password upon registration', async () => {
-    const prismaRegisterService = new PrismaUsersRepository()
-    const registerService = new RegisterService(prismaRegisterService)
+    const usersRepository = new InMemoryUsersRepository()
+
+    const registerService = new RegisterService(usersRepository)
 
     const { user } = await registerService.execute({
       name: 'John Doe',
@@ -20,5 +35,27 @@ describe('Register Service', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+
+    const registerService = new RegisterService(usersRepository)
+
+    const email = 'johndoe@gmail.com'
+
+    await registerService.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerService.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
